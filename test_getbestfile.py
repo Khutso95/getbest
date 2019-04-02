8e341b3bd2c65016ac2b41073c65d5b375a93be0
Khutso Nkadimeng <884200@students.wits.ac.za>
2019-04-02 13:21:50 +0200

    test file

--- test_getbestfile.py
+++ test_getbestfile.py
@@ -1,6 +1,7 @@
 import unittest
 
 class Best_Student(unittest.TestCase):
+	"""Test class"""
 	def test_Index(self):
 		with self.assertRaises(IndexError, "index out of range"):
 			print("index out of range")
